eb46bb265b35facdf0e84381309324328f85a8e2
cacevedo <cacevedo@serempre.com>
2021-09-14 17:52:59 -0500

    Changes on package.json

--- src/components/atoms/typography.tsx
+++ src/components/atoms/typography.tsx
@@ -1,4 +1,4 @@
-import styled, {css, ThemeProps} from 'styled-components';
+import styled, { css, ThemeProps } from 'styled-components';
 
 interface TypographyProps {
   variant?: 'body' | 'title' | 'caption' | 'h1' | 'h2';
@@ -10,38 +10,39 @@ interface TypographyProps {
 
 const body = css`
   font-weight: 400;
-  font-family: "Circular Std";
+  font-family: 'Circular Std';
   font-size: 14px;
   line-height: 1.5;
   color: ${({ theme }) => theme.colors.typography.secondary};
-`
+`;
 
 const h1 = css`
   font-weight: 700;
-  font-family: "Circular Std";
+  font-family: 'Circular Std';
   font-size: 39.81px;
   margin: 0px;
   color: ${({ theme }) => theme.colors.typography.primary};
-`
+`;
 
 const h2 = css`
   font-weight: 400;
-  font-family: "Circular Std";
+  font-family: 'Circular Std';
   color: ${({ theme }) => theme.colors.typography.secondary};
   font-size: 28px;
   line-height: 1.2;
   margin: 0px;
-`
+`;
 
 const caption = css`
   text-transform: uppercase;
   font-size: 13.3px;
   line-height: 1.2;
   font-weight: 400;
-`
+`;
 
 function getColor(color: string, theme: any) {
-  if (['primary', 'secondary'].includes(color)) return theme.colors.typography[color]
+  if (['primary', 'secondary'].includes(color))
+    return theme.colors.typography[color];
   return color;
 }
 
@@ -49,14 +50,26 @@ const variants = {
   body,
   h1,
   h2,
-  caption
-}
+  caption,
+};
 
 const Typography = styled.p<TypographyProps>`
-  ${({ variant }: TypographyProps) => variants[variant as keyof typeof variants || 'body']}
-  ${({ color, ...props }: TypographyProps) => css`color: ${getColor(color, props.theme)};`}
-  ${({ size }: TypographyProps) => size && css`font-size: ${size}px;`}
-  ${({ bolded }: TypographyProps) => bolded && css`font-weight: 500;`}
-`
+  ${({ variant }: TypographyProps) =>
+    variants[(variant as keyof typeof variants) || 'body']}
+  ${({ color, ...props }: TypographyProps) =>
+    css`
+      color: ${getColor(color, props.theme)};
+    `}
+  ${({ size }: TypographyProps) =>
+    size &&
+    css`
+      font-size: ${size}px;
+    `}
+  ${({ bolded }: TypographyProps) =>
+    bolded &&
+    css`
+      font-weight: 500;
+    `}
+`;
 
-export default Typography
+export default Typography;
